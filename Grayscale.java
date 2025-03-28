754fe308eafa7cfe2b7c2b50ef4c68d2d4ccad26
felda-del <felda@my.yorku.ca>
2025-03-28 16:12:50 -0400

    Update Grayscale.java to fix the change in superclass method

--- Grayscale.java
+++ Grayscale.java
@@ -1,37 +1,47 @@
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+
+/**
+ * The Grayscale class is a subclass of the Converter class. It is responsible
+ * for converting a given image to grayscale. It uses BufferedImage to
+ * manipulate pixel values and creates a new image with the grayscale version of
+ * the input image.
+ */
 
 public class Grayscale extends Converter {
-    @Override
-    public void convert(String inputFile, String outputFile) throws IOException {
-        File input = new File(inputFile);
-        BufferedImage img = ImageIO.read(input);
 
-        if (img == null) {
-            throw new IOException("Failed to read image file: " + inputFile);
-        }
+	/**
+	 * Processes the input image and converts it to grayscale.
+	 * 
+	 * This method calculates the grayscale value for each pixel by averaging the
+	 * red, green, and blue components. It then assigns the equivalent grayscale
+	 * value to the RGB values while retaining the original alpha value.
+	 * 
+	 * @param img A BufferedImage representing the input image to be processed
+	 * @return A new BufferedImage representing the grayscale image
+	 */
 
-        processImage(img);
+	@Override
+	public BufferedImage processImage(BufferedImage img) {
 
-        File output = new File(outputFile);
-        ImageIO.write(img, "PNG", output);
-    }
+		// Get width and height of input image
+		int width = img.getWidth();
+		int height = img.getHeight();
 
-    @Override
-    public void processImage(BufferedImage img) {
-        for (int x = 0; x < img.getWidth(); ++x) {
-            for (int y = 0; y < img.getHeight(); ++y) {
-                int rgb = img.getRGB(x, y);
-                ARGB color = new ARGB(rgb);
+		// Create a new grayscale image with the same <width> and <height>
+		BufferedImage processedImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
-                int grayLevel = (color.red + color.green + color.blue) / 3;
-                ARGB grayColor = new ARGB(color.alpha, grayLevel, grayLevel, grayLevel);
+		// Iterate over each pixel to convert it to grayscale
+		for (int x = 0; x < width; ++x) {
+			for (int y = 0; y < height; ++y) {
+				int rgb = img.getRGB(x, y);
+				ARGB color = new ARGB(rgb);
 
-                img.setRGB(x, y, grayColor.toInt());
-            }
-        }
-    }
+				int grayLevel = (color.red + color.green + color.blue) / 3;
+				ARGB grayColor = new ARGB(color.alpha, grayLevel, grayLevel, grayLevel);
+
+				processedImg.setRGB(x, y, grayColor.toInt());
+			}
+		}
+		return processedImg;
+	}
 }
-
